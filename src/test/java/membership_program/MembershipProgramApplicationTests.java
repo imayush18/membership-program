09a4d784822d656c2f951c5fe8e79cd3801c8fae
Ayush Gupta <ayush.akstar@gmail.com>
2026-06-06 12:10:40 +0530

    Initial project setup

---
+++ src/test/java/membership_program/MembershipProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package membership_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
